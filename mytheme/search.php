fd7ddf69378ad47802b0890249127e18c20c6779
jamil <jamil.cse13@gmail.com>
2020-08-25 10:26:48 +0600

    Search Functionality

---
+++ mytheme/search.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<section class="page-wrap">
+<div class="container">
+
+    
+            <h2>Search Result for '<?php echo get_search_query(); ?>'</h2>
+
+            <?php get_template_part('includes/section','searchresults'); ?>
+
+            <?php previous_posts_link(); ?>
+            <?php next_posts_link() ?>
+    
+</div>
+</section>
+
+<?php get_footer(); ?>
